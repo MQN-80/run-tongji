279465071103a5597f70a684fe0187974150bdfb
MQN-80 <3218290253@qq.com>
2022-04-11 21:28:47 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 微信小程序跑步
+
+跟数据库相关操作写在云函数里
+目前实现简单跑步记录和登录页面
+
+## 参考文档
+
+- [云开发文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/getting-started.html)
+
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"E:\tecent1\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-6gnuhn9p12e5fa11 --n quickstartFunctions --r --project "C:\Users\86155\WeChatProjects\miniprogram-1" --report_first --report
